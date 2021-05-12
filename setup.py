7a911d7813b6ac815f9951234243063c27dd2090
Sean Breckenridge <seanbrecke@gmail.com>
2021-05-11 18:33:04 -0700

    v0.1.1: push first 'real' version to pypi
    prototype seems to working fine, good
    to push a real version to pypi now

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = Path("requirements.txt").read_text().strip().splitlines()
 pkg = "browserexport"
 setup(
     name=pkg,
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/seanbreckenridge/browserexport",
     author="Sean Breckenridge",
     author_email="seanbrecke@gmail.com",
