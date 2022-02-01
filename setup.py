612d4d12e98dcedfdd437582a7609a7fad755e25
Sean Breckenridge <seanbrecke@gmail.com>
2022-02-01 10:49:08 -0800

    use sqlite_backup instead of just directly copying db (#25)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = Path("requirements.txt").read_text().strip().splitlines()
 pkg = "browserexport"
 setup(
     name=pkg,
-    version="0.1.3",
+    version="0.2.0",
     url="https://github.com/seanbreckenridge/browserexport",
     author="Sean Breckenridge",
     author_email="seanbrecke@gmail.com",
