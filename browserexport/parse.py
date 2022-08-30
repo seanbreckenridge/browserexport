c2ec26ad5fd7e7348f0e73c47538ccd8e58b5144
Sean Breckenridge <seanbrecke@gmail.com>
2022-08-30 02:02:18 -0700

    allow reading from merged dumps

--- browserexport/parse.py
+++ browserexport/parse.py
@@ -1,11 +1,39 @@
-from typing import Iterator, List, Optional, Type
+from typing import Iterator, List, Optional, Type, Any, Dict
+from pathlib import Path
+from datetime import datetime
 
-from .common import PathIshOrConn
-from .model import Visit
+from .common import PathIshOrConn, PathIsh, expand_path
+from .model import Visit, Metadata
 from .log import logger
 
 from .browsers.common import Browser
 from .browsers.all import DEFAULT_BROWSERS
+
+
+def _read_json_file(path: Path) -> Iterator[Dict[str, Any]]:
+    data: List[Dict[str, Any]]
+    try:
+        # speedup load using orjson if its installed
+        import orjson
+
+        data = orjson.loads(path.read_text())
+
+    except ImportError:
+        import json
+
+        data = json.loads(path.read_text())
+
+    yield from data
+
+
+def _parse_json_file(path: PathIsh) -> Iterator[Visit]:
+    for vjson in _read_json_file(expand_path(path)):
+        metadata_kwargs = vjson["metadata"] or {}
+        yield Visit(
+            url=vjson["url"],
+            dt=datetime.fromtimestamp(vjson["dt"]),
+            metadata=Metadata.make(**metadata_kwargs),
+        )
 
 
 def read_visits(
@@ -17,6 +45,11 @@ def read_visits(
     browsers: List[Type[Browser]] = additional_browsers or []
     browsers += DEFAULT_BROWSERS
     logger.info(f"Reading visits from {path}...")
+
+    if isinstance(path, (str, Path)) and str(path).endswith(".json"):
+        logger.debug("Detected as merged visit JSON dump")
+        yield from _parse_json_file(path)
+        return
     for br in browsers:
         if br.detect(path):
             logger.debug(f"Detected as {br.__name__}")
