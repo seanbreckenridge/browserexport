9902469867c2b2a5d5c4833926be4bf5b4e08397
Sean Breckenridge <seanbrecke@gmail.com>
2023-02-11 14:15:00 -0800

    v0.2.10: add support for flatpak save paths

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = Path("requirements.txt").read_text().strip().splitlines()
 pkg = "browserexport"
 setup(
     name=pkg,
-    version="0.2.9",
+    version="0.2.10",
     url="https://github.com/seanbreckenridge/browserexport",
     author="Sean Breckenridge",
     author_email="seanbrecke@gmail.com",
